f9005cdd35265cbbd4befef83cc42d84cfc4627a
Toufiquer <toufiquerabc@gmail.com>
2025-03-22 04:51:17 +0600

    done fix bug

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -24,12 +24,14 @@ const DashboardPage = () => {
         });
         const result = await response.json();
         if (result.message !== 'Token verified successfully') {
+          localStorage.removeItem('token'); // Clear localStorage if token verification fails
           router.push('/login'); // Redirect to login if token verification fails
         } else {
           setIsLoading(true);
           router.push('/dashboard'); // Redirect to dashboard if token is valid
         }
       } catch {
+        localStorage.removeItem('token'); // Clear localStorage on error
         router.push('/login'); // Redirect to login on error
       }
     };
