4714446453fa26a8468f7cb01423f05bb4d3de5b
Toufiquer <toufiquerabc@gmail.com>
2025-03-25 03:42:47 +0600

    fix bug

---
+++ src/app/components/Slider.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel';
+import Autoplay from 'embla-carousel-autoplay';
+import { UseEmblaCarouselType } from 'embla-carousel-react';
+import Image from 'next/image';
+
+export function Slider() {
+  const [api, setApi] = useState<UseEmblaCarouselType | null>(null);
+  //   const [current, setCurrent] = useState(0);
+  //   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    // setCount(api.scrollSnapList().length);
+
+    // const onSelect = () => {
+    //   setCurrent(api.selectedScrollSnap());
+    // };
+
+    // api.on('select', onSelect);
+    // return () => {
+    //   api.off('select', onSelect);
+    // };
+  }, [api]);
+  const slidersImg = [
+    'https://kanerdul.com/public/664e5dc7d437f.jpg',
+    'https://kanerdul.com/public/664e5dc7d437f.jpg',
+    'https://kanerdul.com/public/664e5dc7d437f.jpg',
+    'https://kanerdul.com/public/664e5dc7d437f.jpg',
+  ];
+  return (
+    <Carousel
+      setApi={api => setApi((api as unknown as UseEmblaCarouselType) || null)} // Ensure proper type handling
+      plugins={[
+        Autoplay({
+          delay: 3000, // Change slide every 3 seconds
+          stopOnInteraction: false, // Continue autoplay even when user interacts
+          jump: false, // Smoothly transition between slides
+        }),
+      ]}
+      className="w-full"
+    >
+      <CarouselContent>
+        {slidersImg.map((imgSrc, index) => (
+          <CarouselItem key={index}>
+            <div className="p-1 w-full flex items-center justify-center h-[200px]">
+              <Image
+                src={imgSrc}
+                alt={`Slide ${index + 1}`}
+                className="h-full w-full object-cover"
+              />
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+    </Carousel>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import AllProducts from './components/AllProducts';
+import { Slider } from './components/Slider';
 
 export default function Home() {
   return (
     <main>
       <div className="container mx-auto px-4">
+        <Slider />
         <AllProducts />
       </div>
     </main>
