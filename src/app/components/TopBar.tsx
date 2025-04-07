0a58d6435d29bfae3c58f2b14a587d2a36b3d838
Toufiquer <toufiquerabc@gmail.com>
2025-04-07 22:58:54 +0600

    done @ merquee and menu

--- src/app/components/TopBar.tsx
+++ src/app/components/TopBar.tsx
@@ -130,7 +130,7 @@ export default function TopBar() {
       {/* Desktop navigation with TopMarquee */}
       {!['/dashboard', '/login'].includes(pathname) && (
         <div className="bg-pink-600 text-white py-2">
-          <div className="container mx-auto flex justify-between items-center px-2">
+          <div className=" container max-w-7xl mx-auto flex justify-between items-center px-2">
             {/* TopMarquee - visible on all devices */}
             <div className="flex-grow md:flex-grow-0 md:w-2/4">
               <TopMarquee />
@@ -159,13 +159,13 @@ export default function TopBar() {
       )}
 
       {/* Search Bar - visible on all devices */}
-      <div className="bg-white shadow-md py-3 px-4">
-        <div className="container mx-auto flex items-center justify-between">
+      <div className="bg-white shadow-md py-3 md:px-4 px-1">
+        <div className=" container max-w-7xl mx-auto flex items-center justify-between">
           {/* Logo */}
           <div className="flex items-center">
             <Link href="/" className="text-2xl font-bold text-pink-600">
               <div className="w-[160px] h-[30px] relative">
-                <div className="absolute top-[-15px] left-0 w-full h-auto mb-4">
+                <div className="absolute top-[-15px] left-[-10px] md:left-0 w-full h-auto mb-4">
                   <Image
                     src="/rupushi-crop.png" // Replace with your actual logo path
                     alt="Rupush Logo"
@@ -221,22 +221,6 @@ export default function TopBar() {
           </div>
           {/* Desktop User Icons */}
           <div className="hidden md:flex items-center space-x-4">
-            <Link href="/login" className="text-gray-700 hover:text-pink-600">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-                />
-              </svg>
-            </Link>
             <Link href="/wishlist" className="text-gray-700 hover:text-pink-600">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -304,96 +288,120 @@ export default function TopBar() {
 
       {/* Mobile Menu */}
       {isMenuOpen && (
-        <div className="md:hidden bg-white shadow-lg transition-transform transform duration-300 ease-in-out translate-y-0">
-          <div className="px-4 py-3 border-b">
-            <input
-              type="text"
-              placeholder="আপনার পছন্দের পণ্য খুঁজুন..."
-              className="w-full text-gray-800 px-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-pink-500"
-            />
-            <div className="pt-2">
-              <div className="px-4 text-center py-2 rounded-full border border-green-600 text-green-600 hover:bg-green-600 hover:text-white transition duration-200">
-                খুঁজুন...
+        <div className="w-full h-screen fixed bg-slate-50 z-40">
+          <div className="w-full flex items-center justify-end p-2">
+            <button onClick={toggleMenu} className="text-pink-600 focus:outline-none">
+              {/* ক্রস আইকন */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+          <div className="md:hidden bg-white shadow-lg transition-transform transform duration-300 ease-in-out translate-y-0">
+            <div className="px-4 py-3 border-b">
+              <input
+                type="text"
+                placeholder="আপনার পছন্দের পণ্য খুঁজুন..."
+                className="w-full text-gray-800 px-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-pink-500"
+              />
+              <div className="pt-2">
+                <div className="px-4 text-center py-2 rounded-full border border-green-600 text-green-600 hover:bg-green-600 hover:text-white transition duration-200">
+                  খুঁজুন...
+                </div>
               </div>
             </div>
-          </div>
-          <nav className="px-4 py-2">
-            <Link href="/" className="block py-2 text-gray-800 hover:text-pink-600">
-              Home
-            </Link>
-            <Link href="/about-us" className="block py-2 text-gray-800 hover:text-pink-600">
-              About Us
-            </Link>
-            <Link href="/terms-condition" className="block py-2 text-gray-800 hover:text-pink-600">
-              Terms & Condition
-            </Link>
-            <Link href="/privacy-policy" className="block py-2 text-gray-800 hover:text-pink-600">
-              Privacy Policy
-            </Link>
+            <nav className="px-4 py-2">
+              <Link href="/" className="block py-2 text-gray-800 hover:text-pink-600">
+                Home
+              </Link>
+              <Link href="/about-us" className="block py-2 text-gray-800 hover:text-pink-600">
+                About Us
+              </Link>
+              <Link
+                href="/terms-condition"
+                className="block py-2 text-gray-800 hover:text-pink-600"
+              >
+                Terms & Condition
+              </Link>
+              <Link href="/privacy-policy" className="block py-2 text-gray-800 hover:text-pink-600">
+                Privacy Policy
+              </Link>
 
-            {/* Category links added to mobile menu */}
-            <div className="mt-2 pt-2 border-t border-gray-200">
-              <h3 className="font-semibold text-gray-800 mb-2">ক্যাটাগরি</h3>
-              {categoryMenuItems.map((item, index) => (
-                <Link
-                  href={item.href}
-                  key={index}
-                  className="block py-2 text-gray-800 hover:text-pink-600"
+              {/* Category links added to mobile menu */}
+              <div className="mt-2 pt-2 border-t border-gray-200">
+                <h3 className="font-semibold text-gray-800 mb-2">ক্যাটাগরি</h3>
+                {categoryMenuItems.map((item, index) => (
+                  <Link
+                    href={item.href}
+                    key={index}
+                    className="block py-2 text-gray-800 hover:text-pink-600"
+                  >
+                    {item.name}
+                  </Link>
+                ))}
+              </div>
+            </nav>
+            <div className="flex justify-around py-3 border-t">
+              <Link href="/login" className="text-gray-700 hover:text-pink-600">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
                 >
-                  {item.name}
-                </Link>
-              ))}
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                  />
+                </svg>
+              </Link>
+              <Link href="/wishlist" className="text-gray-700 hover:text-pink-600">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                  />
+                </svg>
+              </Link>
+              <Link href="/cart" className="text-gray-700 hover:text-pink-600">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                  />
+                </svg>
+              </Link>
             </div>
-          </nav>
-          <div className="flex justify-around py-3 border-t">
-            <Link href="/login" className="text-gray-700 hover:text-pink-600">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-                />
-              </svg>
-            </Link>
-            <Link href="/wishlist" className="text-gray-700 hover:text-pink-600">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                />
-              </svg>
-            </Link>
-            <Link href="/cart" className="text-gray-700 hover:text-pink-600">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                />
-              </svg>
-            </Link>
           </div>
         </div>
       )}
