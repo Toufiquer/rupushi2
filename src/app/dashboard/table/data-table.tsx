ecb5317045ca1982d59525043867c676d06ab0e0
Toufiquer <toufiquerabc@gmail.com>
2025-04-08 13:53:29 +0600

    done

--- src/app/dashboard/table/data-table.tsx
+++ src/app/dashboard/table/data-table.tsx
@@ -65,6 +65,13 @@ const DataTable = <TData, TValue>({
       pagination: {
         pageSize: 5,
       },
+      sorting: [
+        {
+          // Replace 'createdAt' with your actual timestamp field name
+          id: 'createdAt',
+          desc: true, // true for descending order (newest first)
+        },
+      ],
     },
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
