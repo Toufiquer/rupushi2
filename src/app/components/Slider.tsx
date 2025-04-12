6829350e64e76451da405bec3e5c37dd7b1602a0
Toufiquer <toufiquerabc@gmail.com>
2025-04-12 21:41:23 +0600

    done search box

--- src/app/components/Slider.tsx
+++ src/app/components/Slider.tsx
@@ -18,43 +18,46 @@ export function Slider() {
     'https://i.ibb.co.com/qM0tdtXk/banner-1.jpg',
     'https://i.ibb.co.com/VcP5bBLL/banner-2.jpg',
   ];
+
   return (
-    <Carousel
-      plugins={[
-        Autoplay({
-          delay: 3000, // Change slide every 3 seconds
-          stopOnInteraction: false, // Continue autoplay even when user interacts
-          jump: false, // Smoothly transition between slides
-          playOnInit: true,
-        }),
-      ]}
-      className="w-full"
-    >
-      <CarouselContent>
-        {[
-          ...slidersImg,
-          ...slidersImg,
-          ...slidersImg,
-          ...slidersImg,
-          ...slidersImg,
-          ...slidersImg,
-          ...slidersImg,
-          ...slidersImg,
-          ...slidersImg,
-        ].map((imgSrc, index) => (
-          <CarouselItem key={index}>
-            <div className="p-1 w-full flex items-center justify-center h-[260px]">
-              <Image
-                width={1300}
-                height={260}
-                src={imgSrc}
-                alt={`Slide ${index + 1}`}
-                className="h-full w-full object-fill"
-              />
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-    </Carousel>
+    <div className="w-full mx-auto max-w-screen-2xl px-4">
+      <Carousel
+        plugins={[
+          Autoplay({
+            delay: 3000,
+            stopOnInteraction: false,
+            jump: false,
+            playOnInit: true,
+          }),
+        ]}
+        className="w-full"
+        opts={{
+          loop: true,
+          align: 'center',
+        }}
+      >
+        <CarouselContent>
+          {slidersImg.map((imgSrc, index) => (
+            <CarouselItem key={index}>
+              <div className="w-full flex items-center justify-center">
+                <div className="relative w-full h-auto overflow-hidden rounded-lg">
+                  {/* Fixed height container that adjusts by screen size */}
+                  <div className="h-[100px] xs:h-[140px] md:h-[260px] lg:h-96 w-full flex items-center justify-center">
+                    <Image
+                      src={imgSrc}
+                      alt={`Slide ${index + 1}`}
+                      width={1300}
+                      height={500}
+                      className="object-contain max-h-full max-w-full"
+                      priority={index < 2}
+                    />
+                  </div>
+                </div>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+    </div>
   );
 }
