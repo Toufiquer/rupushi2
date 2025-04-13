b0e9e81c12a769502a16b833ce2f7d8107e3851e
Toufiquer <toufiquerabc@gmail.com>
2025-04-13 09:15:08 +0600

    done @ design

--- src/app/receipt/[...orderId]/page.tsx
+++ src/app/receipt/[...orderId]/page.tsx
@@ -19,11 +19,20 @@ const Page = () => {
     if (orderId) {
       fetch(`/api/v1/orders/receipt/${orderId}`)
         .then(d => d.json())
-        .then(data => console.log('data : ', data));
+        .then(data => {
+          console.log('data : ', data.data);
+          setOrder(data.data);
+        });
     }
   }, [orderId]);
+  const handlePrint = () => {
+    console.log('order : ', order);
+  };
   return (
-    <main className="bg-slate-800 text-white flex items-center justify-center w-full h-screen">
+    <main className="bg-slate-800 flex-col text-white flex items-center justify-center w-full h-screen">
+      <button className="" onClick={handlePrint}>
+        Print
+      </button>
       {orderId && (
         <div>
           <h1>Receipt for Order: {Array.isArray(orderId) ? orderId.join('/') : orderId}</h1>
