42d0c8792dd48d5a41633647a81ae081634c508f
Toufiquer <toufiquerabc@gmail.com>
2025-06-14 00:37:31 +0600

    done @ get images

--- src/app/lucky-gifts-campaign/components/logo.tsx
+++ src/app/lucky-gifts-campaign/components/logo.tsx
@@ -10,10 +10,10 @@ import Image from 'next/image';
 
 const LogoComponent = () => {
   return (
-    <div className="w-sm h-40 relative mx-auto mt-4">
+    <div className="w-sm h-40 relative mx-auto my-4">
       <div className="absolute top-0 left-0">
         <Image
-          src="/rupushi-crop.png" // Replace with your actual logo path
+          src="/rupushi-logo-white.png" // Replace with your actual logo path
           alt="Rupush Logo"
           width={1200}
           height={1000}
