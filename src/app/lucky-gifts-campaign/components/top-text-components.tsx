34f5f1533504c01cadd7751e7357d2aa62ae1e09
Toufiquer <toufiquerabc@gmail.com>
2025-06-14 19:39:30 +0600

    update text

--- src/app/lucky-gifts-campaign/components/top-text-components.tsx
+++ src/app/lucky-gifts-campaign/components/top-text-components.tsx
@@ -11,11 +11,11 @@ const TopTextComponents = () => {
     <main className="">
       <div className="w-full bg-slate-50 rounded-lg">
         <h2 className="my-kalpurush-text font-normal text-2xl p-4 text-center leading-10 md:leading-13">
-          মাত্র ৬৯৯ টাকায় দারুন ডিজাইনের <span className={textRed}>জুয়েলারি সেট! </span> স্টাইলিশ
-          <span className={textRed}> কাঠের বক্স,</span> সাথে
+          মাত্র ৬৯৯ টাকায় দারুন ডিজাইনের <span className={textRed}>জুয়েলারি সেট! </span> অর্ডার
+          করলেই
           <span className={textRed}> ১০ লক্ষ টাকা পুরস্কার</span> জেতার সুযোগ! <br />
           <span className={textRed}> প্রথম ১০ জন পাচ্ছেন ১০ টি আকর্ষণীয় গিফট!</span> বাকি
-          <span className={textRed}> ১০০ জন বিজয়ী পাচ্ছেন </span> প্রতি জনে
+          <span className={textRed}> ১০০ জন পাচ্ছেন </span> প্রতি জনে
           <span className={textRed}>
             {' '}
             ৫০০০ টাকা করে ক্যাশ! <br /> সর্বমোট বিজয়ী হবেন ১১০ জন!
